fbda163808f9d6b856b28b9588142d8c2b4ed280
Austin Chiatto <chiattoaustin@gmail.com>
2024-03-23 19:27:07 -0700

    Implement static site header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import Typography from '../Typography/Typography';
+import styles from './header.module.css';
+
+const Header = () => {
+  return (
+    <section className={styles.header}>
+      <div className={styles.intro}>
+        <h1>Apogee</h1>
+        <Typography
+          level={2}
+          style="p"
+          color="secondary"
+        >
+          Your window to the space race.
+        </Typography>
+      </div>
+      <Typography color="secondary">
+        A live feed of planned missions in spaceflight <br /> from agencies around the world.
+      </Typography>
+    </section>
+  );
+};
+
+export default Header;
