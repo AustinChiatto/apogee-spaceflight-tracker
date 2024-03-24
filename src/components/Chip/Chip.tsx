890d16369d02d25398659da3b2d96de1ef946173
Austin Chiatto <chiattoaustin@gmail.com>
2024-03-23 22:54:08 -0700

    Add Chip component

---
+++ src/components/Chip/Chip.tsx
@@ -0,0 +1,23 @@
+import styles from './chip.module.css';
+import { Roboto_Mono } from 'next/font/google';
+
+type ChipProps = {
+  label: string;
+  isGlass?: boolean;
+  style?: React.CSSProperties;
+};
+
+const mono = Roboto_Mono({ subsets: ['latin'] });
+
+const Chip = ({ label, isGlass, style }: ChipProps) => {
+  return (
+    <div
+      className={`${isGlass ? styles.chipGlass : styles.chipSolid} ${mono.className}`}
+      style={style}
+    >
+      {label}
+    </div>
+  );
+};
+
+export default Chip;
