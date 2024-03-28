ba65abd3ff21fbc77d7d89ca128b52e410416202
Austin Chiatto <chiattoaustin@gmail.com>
2024-03-27 18:12:11 -0700

    Implement new persistent close button to modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -155,39 +155,44 @@ const Modal = ({ handleToggleModal, modalData }: modalProps) => {
   );
 
   return (
-    <div
-      className={`${styles.modalWrapper} hasModal`}
-      role="dialog"
-      onClick={handleOffModalClick}
-    >
-      <div
-        className={styles.modalContainer}
-        ref={modalRef}
+    <>
+      <button
+        className={styles.modalNavWrapper}
+        onClick={handleOffModalClick}
       >
-        <div className={styles.modalHeader}>
-          <Chip
-            label="T- 0 Days 00:00:00"
-            style={{ padding: '0.375rem 0.75rem', fontSize: '1.25rem' }}
+        <div className={styles.modalNavInner}>
+          <Image
+            src="/close-icon.svg"
+            alt="close-icon"
+            width={16}
+            height={16}
           />
-          <button
-            className={styles.modalCloseButton}
-            onClick={handleToggleModal}
-          >
-            <Image
-              src="/close-icon.svg"
-              alt="close icon"
-              width={16}
-              height={16}
-            />
-          </button>
+          Close
         </div>
-        <div className={styles.modalContent}>
-          {MissionSection}
-          {VehicleSection}
-          {agency && AgencySection}
+      </button>
+      <div
+        className={`${styles.modalWrapper} hasModal`}
+        role="dialog"
+        onClick={handleOffModalClick}
+      >
+        <div
+          className={styles.modalContainer}
+          ref={modalRef}
+        >
+          <div className={styles.modalHeader}>
+            <Chip
+              label="T- 0 Days 00:00:00"
+              style={{ padding: '0.375rem 0.75rem', fontSize: '1.25rem' }}
+            />
+          </div>
+          <div className={styles.modalContent}>
+            {MissionSection}
+            {VehicleSection}
+            {agency && AgencySection}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
